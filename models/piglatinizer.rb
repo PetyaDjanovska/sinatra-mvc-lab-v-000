4ab6a5cd179aba637925afc7aa84f8a5f438a8f7
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-09 23:04:18 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,15 +4,17 @@ class PigLatinizer
   def initialize
   end
 
-  def piglatinize (word)
+  def piglatinize(word)
     vowels = "aeiouAEIOU"
       if vowels.include?(word[0])
         word = word + "way"
       else
-        word.each_char.with_index do |l,i|
-            while !vowels.include?(l) do
-              word = word[1..-1] + word[i] + "ay"
-            end
+        word.each_char do |l|
+          if !vowels.include?(l)
+            word = word[1..-1] + word[0]
+          else
+            word += "ay"
+            break
           end
         end
       end
