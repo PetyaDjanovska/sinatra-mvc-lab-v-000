509d53782c3b0355f439360f42c5ea21818c7856
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-09 23:25:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinize' do
+  post '/' do
     @converted_text = PigLatinizer.new(params[:user_phrase])
 
     erb :piglatinized
